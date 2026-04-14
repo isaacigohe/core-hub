a8b9c6d79301c0cc07b291cdbcd237b324f02a5f
isaacigohe <isaacigohe120@gmail.com>
2026-04-14 18:02:26 +0300

    Add new file

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2>🎥 MovieApp</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Explore</li>
+        <li>Favorites</li>
+        <li>Watched</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
