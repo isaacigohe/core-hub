595650c6c73cbb842db8b8be66ef32b0891a36f0
isaacigohe <isaacigohe120@gmail.com>
2026-04-15 13:49:09 +0300

    setup: the code layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import Search from "./pages/search";
+import Search from "./pages/Search";
 import Auth from "./pages/Auth";
+import { auth } from "./services/firebase";
+
+function PrivateRoute({ children }) {
+  return auth.currentUser ? children : <Navigate to="/auth" />;
+}
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/search" element={<Search />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/search" element={<Search />} />
+
+        {/* PROTECTED ROUTE */}
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
